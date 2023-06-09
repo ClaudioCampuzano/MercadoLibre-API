e10544e08cc560e41dbd186af996824bb78faf3d
Claudio <claudio.campuzano.b@gmail.com>
2023-06-09 08:59:09 -0400

    feat: add code to configure API

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import express from 'express'
-import cors from "cors"
+import express from 'express';
+import cors from 'cors';
+import { router } from './routes';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-app.get('/', (_req, res) => {
-    res.json({'message': 'ok'});
-  })
-
-const PORT = 12345
+const PORT = 3456;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
-})
+  console.log(`Server running on ${PORT}`);
+});
